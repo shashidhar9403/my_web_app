b716b658b4127827f3e1062de886481d41b8b175
shashidhar9403 <shashidhar9403@gmail.com>
2021-12-21 18:24:37 +0000

    Local Development

--- services/web/src/server.js
+++ services/web/src/server.js
@@ -1,3 +1,4 @@
+import http from 'http';
 import sirv from 'sirv';
 import polka from 'polka';
 import compression from 'compression';
@@ -6,10 +7,25 @@ import * as sapper from '@sapper/server';
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === 'development';
 
+const devFirebaseServe = (req, res, next) => {
+	if (dev && req.url.startsWith("/__/")) {
+	  http.get({
+		hostname: "localhost",
+		port: 5000,
+		path: req.url
+	  }, (firebaseServeResponse) => {
+		firebaseServeResponse.pipe(res);
+	  });
+	} else {
+	  next();
+	}
+};
+
 polka() // You can also use Express
 	.use(
 		compression({ threshold: 0 }),
 		sirv('static', { dev }),
+		devFirebaseServe,
 		sapper.middleware()
 	)
 	.listen(PORT, err => {
